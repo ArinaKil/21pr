49b01af42890a632ce8f243675140d3acbf2a250
ArinaKil <arikakilka@mail.ru>
2026-06-11 12:52:13 +0500

    Программирование окна MainWindow. Килунина

--- RegIN_Kilunina/MainWindow.xaml.cs
+++ RegIN_Kilunina/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RegIN_Kilunina.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,20 +10,40 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RegIN_Kilunina
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public User UserLogIn = new User();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
+            OpenPage(new Pages.Login());
+        }
+
+        public void OpenPage(Page page) 
+        {
+            DoubleAnimation StartAnimation = new DoubleAnimation();
+            StartAnimation.From = 1;
+            StartAnimation.To = 0;
+            StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            StartAnimation.Completed += delegate
+            {
+                frame.Navigate(page);
+                DoubleAnimation EndAnimation = new DoubleAnimation();
+                EndAnimation.From = 0;
+                EndAnimation.To = 1;
+                EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, EndAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, StartAnimation);
         }
     }
 }
