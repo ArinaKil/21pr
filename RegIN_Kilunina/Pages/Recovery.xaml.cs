1f6483902152f3c8630480d0bb63ce65b3960d0e
ArinaKil <arikakilka@mail.ru>
2026-06-12 11:26:33 +0500

    Разработанная страница Recovery. Килунина

---
+++ RegIN_Kilunina/Pages/Recovery.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegIN_Kilunina.Pages
+{
+    public partial class Recovery : Page
+    {
+        public Recovery()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLogin(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void SetLogin(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
